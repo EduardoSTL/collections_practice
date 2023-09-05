01d4c5104e0affd6dd019d0430134501180e8469
root <eduardojosemelgar2004@gmail.com>
2023-09-04 23:29:18 -0600

    feat: print sets

--- bin/set_practice.dart
+++ bin/set_practice.dart
@@ -1,3 +1,10 @@
 void main() {
-  print('=== EJEMPLO SET ===')
+  print('=== EJEMPLO SET ===');
+  //representa una coleccion de elementos unicos, no pueden haber DUPLICADOS
+  Set<String> week = {'Lunes','Martes','Miercoles','Jueves','Viernes'};
+  Set<String> weekend = {'Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo'};
+  print('Dias de la semana: $week');
+  print('semana + fin de semana: $weekend');
+
+
 }
