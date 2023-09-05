9661f7bffab423f2962577ba3b2289f73fb16100
root <eduardojosemelgar2004@gmail.com>
2023-09-04 21:51:55 -0600

    feat: print names en orden alfabetico

--- bin/list_practice.dart
+++ bin/list_practice.dart
@@ -1,8 +1,15 @@
 void main() {
   print('=== LIST EJEMPLO ===');
-  List<String> names = ['Juan', 'Rodolfo', 'Manuel'];
+  List<String> names = ['Juan','Rodolfo','Manuel','Jose','Alex','Oscar'];
   print('Nombres existentes: $names');
 
   print('Primer nombre en la lista: ${names.first}');
   print('Ultimo nombre de la lista: ${names.last}');
+
+  //ordenar alfabeticamente
+  names.sort();
+  print('Nombres existentes (orden alfabético): ');
+  for(String orden in names){
+    print(orden);
+  }
 }
