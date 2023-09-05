1ef3067247a2c28d34ad918c207732660589d193
root <eduardojosemelgar2004@gmail.com>
2023-09-04 21:40:17 -0600

    first commit

---
+++ bin/list_practice.dart
@@ -0,0 +1,8 @@
+void main() {
+  print('=== LIST EJEMPLO ===');
+  List<String> names = ['Juan', 'Rodolfo', 'Manuel'];
+  print('Nombres existentes: $names');
+
+  print('Primer nombre en la lista: ${names.first}');
+  print('Ultimo nombre de la lista: ${names.last}');
+}
---
+++ test/collections_practice_test.dart
@@ -0,0 +1,8 @@
+import 'package:collections_practice/collections_practice.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
