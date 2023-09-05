9ef87c5a115da31c0d323b1c877adf606f745c7b
root <eduardojosemelgar2004@gmail.com>
2023-09-04 23:49:56 -0600

    update: iterable

--- bin/iterable_practice.dart
+++ bin/iterable_practice.dart
@@ -1,3 +1,9 @@
 void main() {
   print('=== EJEMPLO ITERABLE ===');
+  //es una clase abstracta que representa una coleccion de elementos
+  //a los que se accede secuencialmente
+  Iterable<int> iterable = [1,2,3,4,6];
+  int value = iterable.elementAt(1);
+  print('Imprimir valor en el indice 1: $value');
+  print('Tamaño de la lista: ${iterable.length}');
 }
